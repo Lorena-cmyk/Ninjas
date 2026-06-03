5ae9fdc4d589c22a5f885d8ea2c77e7c24d65b9a
lorena <lorenacoelhomoreira765@gmail.com>
2026-06-02 22:44:34 -0300

    Implementacao de validacoes

--- src/main/java/br/com/etec/ninjas/model/Ninjas.java
+++ src/main/java/br/com/etec/ninjas/model/Ninjas.java
@@ -1,16 +1,23 @@
 package br.com.etec.ninjas.model;
 
+import org.hibernate.validator.constraints.br.CPF;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Entity
 @Table (name = "ninja")//Nome diferente do nome da nossa classe 
 @Data
+@Valid
 
 //Construtores vazios 
 public class Ninjas {
@@ -19,10 +26,14 @@ public class Ninjas {
     private Long id_ninja;
                                 //nullable = nao pode ser nulo
     @Column(name = "nome_ninja", nullable = false)//fica em cima, é a anotacao e quem vai receber a regra
+    @NotBlank(message = "Nome é obrigatório!")//NotBlanck é utilizada para validar campos de texto
+    @Size(min = 3, max = 255, message = "Nome deve ter entre 3 a 255 caracteres.")
     private String nome;
     @Column(name = "cpf_ninja", nullable = false, unique = true)
+    @CPF
     private String cpf;
     @Column(name = "email_ninja", nullable = false, unique = true)
+    @Email(message = "Email inválido.")
     private String email;
      
     
